93e66bb5da034eda31ed38ef3dd36056364c3414
Sahajadi <sahajadilohani123@gmail.com>
2025-11-19 11:34:58 +0545

    task4 completed

--- dotnet_workshop_w4/Program.cs
+++ dotnet_workshop_w4/Program.cs
@@ -88,3 +88,32 @@
 //    }
 //}
 
+// task4
+using System;
+
+namespace dotnet_workshop_w4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Object using default constructor
+            Task4 player1 = new Task4();
+            // Assign values manually
+            player1.playerName = "Default Player";
+            player1.level = 1;
+            player1.health = 100;
+            Console.WriteLine("Player 1 (Default Constructor):");
+            player1.DisplayInfo();
+
+            // Object using parameterized constructor
+            Task4 player2 = new Task4("Sahajadi", 5, 150);
+            Console.WriteLine("Player 2 (Parameterized Constructor):");
+            player2.DisplayInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
---
+++ dotnet_workshop_w4/Task4.cs
@@ -0,0 +1,36 @@
+﻿// Task4.cs
+using System;
+
+namespace dotnet_workshop_w4
+{
+    internal class Task4
+    {
+        // Instance fields
+        public string playerName;
+        public int level;
+        public int health;
+
+        // Default constructor
+        public Task4()
+        {
+            Console.WriteLine("Default constructor has been called");
+        }
+
+        // Parameterized constructor
+        public Task4(string name, int lvl, int hp)
+        {
+            playerName = name;
+            level = lvl;
+            health = hp;
+        }
+
+        // Method to display player info
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Player Name: " + playerName);
+            Console.WriteLine("Level: " + level);
+            Console.WriteLine("Health: " + health);
+            Console.WriteLine();
+        }
+    }
+}
