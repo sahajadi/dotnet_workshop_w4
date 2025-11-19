f2d90c019d3f9e21d54c4e0933ddd5a83e615a76
Sahajadi <sahajadilohani123@gmail.com>
2025-11-19 11:12:37 +0545

    Add project files.

---
+++ dotnet_workshop_w4/Program.cs
@@ -0,0 +1,20 @@
+﻿//using System;
+
+//namespace dotnet_workshop_w4
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // Create students using the constructor
+//            Student student1 = new Student("Sahajadi", 21, "Information Technology");
+//            Student student2 = new Student("Aarohi", 20, "Information Technology");
+
+//            // Display their info
+//            student1.DisplayInfo();
+//            student2.DisplayInfo();
+
+//            Console.ReadLine();
+//        }
+//    }
+//}
---
+++ dotnet_workshop_w4/student.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+
+//namespace dotnet_workshop_w4
+//{
+//    internal class Student
+//    {
+//        // Instance fields
+//        public string Name;
+//        public int Age;
+//        public string Course;
+
+//        // Static field
+//        public static string SchoolName = "Itahari International College";
+
+//        // Constructor to set instance fields quickly
+//        public Student(string name, int age, string course)
+//        {
+//            Name = name;
+//            Age = age;
+//            Course = course;
+//        }
+
+//        // Method to display student info
+//        public void DisplayInfo()
+//        {
+//            Console.WriteLine("Name: " + Name);
+//            Console.WriteLine("Age: " + Age);
+//            Console.WriteLine("Course: " + Course);
+//            Console.WriteLine("School: " + SchoolName);
+//            Console.WriteLine();
+//        }
+//    }
+//}
