050df630a198c2c0ada940838b6f93f24d5c882c
Sahajadi <sahajadilohani123@gmail.com>
2025-11-19 11:20:13 +0545

    task 2 completed

--- dotnet_workshop_w4/Program.cs
+++ dotnet_workshop_w4/Program.cs
@@ -1,4 +1,6 @@
-﻿//using System;
+﻿
+//// task1
+//using System;
 
 //namespace dotnet_workshop_w4
 //{
@@ -18,3 +20,38 @@
 //        }
 //    }
 //}
+
+
+//// task2
+//using System;
+
+//namespace dotnet_workshop_w4
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // Create Calculator object
+//            Calculator calc = new Calculator();
+
+//            // Call PrintWelcome
+//            calc.PrintWelcome();
+//            Console.WriteLine();
+
+//            // Call Add method
+//            int sum = calc.Add(10, 20);
+//            Console.WriteLine("Addition of 10 and 20: " + sum);
+
+//            // Call Multiply method with both numbers
+//            int product1 = calc.Multiply(5, 4);
+//            Console.WriteLine("Multiplication of 5 and 4: " + product1);
+
+//            // Call Multiply method with only one number (num2 defaults to 1)
+//            int product2 = calc.Multiply(7);
+//            Console.WriteLine("Multiplication of 7 and default 1: " + product2);
+
+//            Console.ReadLine();
+//        }
+//    }
+//}
+ 
---
+++ dotnet_workshop_w4/Task2.cs
@@ -0,0 +1,26 @@
+﻿// Calculator.cs
+using System;
+
+namespace dotnet_workshop_w4
+{
+    internal class Calculator
+    {
+        // Void method to print welcome message
+        public void PrintWelcome()
+        {
+            Console.WriteLine("Welcome to the Calculator");
+        }
+
+        // Method to add two numbers
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        // Method to multiply two numbers, second parameter optional
+        public int Multiply(int num1, int num2 = 1)
+        {
+            return num1 * num2;
+        }
+    }
+}
